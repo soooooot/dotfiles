504189e6f57183b14d367cda40b73a1ab839633c
soooooot <sooooootZHOU@gmail.com>
2015-11-01 14:17:03 +0800

    initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing dotfiles"
+
+# echo "Initializing submodule(s)"
+# git submodule update --init --recursive
+
+
+echo "install link.sh"
+source install/link.sh
+
+
+echo "Done."
